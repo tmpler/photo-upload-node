cf6b859bbcd3c4b2ec9444b2ccdc445637267934
tmpler <mean@jsdev.uk>
2017-05-19 09:48:45 +0100

    dummy photos

---
+++ routes/photos.js
@@ -0,0 +1,9 @@
+var photos = [];
+photos.push({
+  name: 'Placeholder 300x300',
+  path: 'http://placehold.it/300x300'
+});
+photos.push({
+  name: 'Placeholder 400x300',
+  path: 'http://placehold.it/400x300'
+});
