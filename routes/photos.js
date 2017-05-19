2126480130110728619faff94d158cb62626fa7a
tmpler <mean@jsdev.uk>
2017-05-19 09:49:53 +0100

    list route

--- routes/photos.js
+++ routes/photos.js
@@ -7,3 +7,9 @@ photos.push({
   name: 'Placeholder 400x300',
   path: 'http://placehold.it/400x300'
 });
+exports.list = function(req, res){
+    res.render('photos', {
+      title: 'Photos',
+      photos: photos
+    });
+};
