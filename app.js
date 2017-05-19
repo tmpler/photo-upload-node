e1f9e403f44a2f45b367cd7f5cf97ba94816ddde
tmpler <mean@jsdev.uk>
2017-05-19 10:50:10 +0100

    photo model and routes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.set('photos', path.join(__dirname, 'public', 'photos'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,9 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', photo.list);
+app.use('/', photos.list);
 app.use('/users', users);
-
+app.get('/upload', photos.form);
+app.post('/upload', photos.submit(app.get('photos')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
